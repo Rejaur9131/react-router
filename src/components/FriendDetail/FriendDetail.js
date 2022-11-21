3d6a814467a12f32d84b56c710cab4a80daddc47
MD REJAUR RAHMAN <you@example.com>
2022-11-21 09:06:43 +0600

    worked with loading data for frienDetail and learned optional chaining

--- src/components/FriendDetail/FriendDetail.js
+++ src/components/FriendDetail/FriendDetail.js
@@ -1,13 +1,30 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 
 const FriendDetail = () => {
   const { friendId } = useParams();
   // const params = useParams();
+  const [friend, setFriend] = useState({});
+
+  useEffect(() => {
+    axios
+      .get(`https://jsonplaceholder.typicode.com/users/${friendId}`)
+      .then((data) => setFriend(data.data));
+  }, []);
   return (
     <div>
       {/* <h2>This is details of a Bondhu :{params.friendId} </h2> */}
       <h2>This is details of a Bondhu :{friendId} </h2>
+      <h3>Name: {friend.name}</h3>
+      <h4>Email: {friend.email}</h4>
+      <h6>Wesite: {friend.website}</h6>
+      <p>
+        <small>City: {friend.address?.city}</small>
+      </p>
+      <p>
+        <small>Lat: {friend.address?.geo?.lat}</small>
+      </p>
     </div>
   );
 };
